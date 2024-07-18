f450061d05a05685450e178ebc6ca9afd06a8921
hyeongjeongyoo <hjeong0711@gmail.com>
2024-07-18 15:16:47 +0900

    교수 강의디테일 레파지토리 작성

---
+++ src/main/java/com/tenco/group3/repository/StuSubDetailRepository.java
@@ -0,0 +1,8 @@
+package com.tenco.group3.repository;
+
+import com.tenco.group3.model.StuSubDetail;
+
+public interface StuSubDetailRepository {
+
+	StuSubDetail getStuSubStuSubDetailByStudentId(int id);
+}
