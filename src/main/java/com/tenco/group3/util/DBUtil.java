0321980003774422e413425138c189d0d9f909cf
dbstjlee <dbstjlee@naver.com>
2024-07-18 14:12:07 +0900

    데이터베이스 세팅

---
+++ src/main/java/com/tenco/group3/util/DBUtil.java
@@ -0,0 +1,28 @@
+package com.tenco.group3.util;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class DBUtil {
+
+	private static DataSource datesource;
+	
+	static {
+		try {
+			InitialContext ctx = new InitialContext();
+			datesource = (DataSource) ctx.lookup("java:comp/env/jdbc/greenboard");
+		} catch (NamingException e) {
+			System.out.println("DBUtil 초기화 실패");
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection getConnetion() throws SQLException {
+		return datesource.getConnection();
+	}
+	
+}
