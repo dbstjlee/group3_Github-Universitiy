6917e8125eb2dcde381c36dea8013fb6b258e895
MH <alsgur6811@gmail.com>
2024-07-18 15:17:07 +0900

    interfaces

--- src/main/java/com/tenco/group3/model/Subject.java
+++ src/main/java/com/tenco/group3/model/Subject.java
@@ -13,17 +13,17 @@ import lombok.ToString;
 @Builder
 public class Subject {
 	private int id;
-	private String name;
-	private int professor_id;
-	private String room_id;
-	private int dept_id;
-	private String type;
-	private int sub_year;
-	private int semester;
-	private String sub_day;
-	private int start_time;
-	private int end_time;
-	private int grades;
-	private int capacity;
-	private int num_of_student;
+	private String name; // 수업 명
+	private int professor_id; // 교수 ID
+	private String room_id; // 강의실 ID
+	private int dept_id; // 학과 ID
+	private String type; // 전공, 교양
+	private int sub_year; // 년도
+	private int semester; // 학기
+	private String sub_day; // 요일
+	private int start_time; // 시작 시간
+	private int end_time; // 종료 시간
+	private int grades; // 학점
+	private int capacity; // 학생 정원
+	private int num_of_student; // 신청 학생수
 }
---
+++ src/main/java/com/tenco/group3/repository/interfaces/SubjectRepository.java
@@ -0,0 +1,21 @@
+package com.tenco.group3.repository.interfaces;
+
+import java.util.List;
+
+import com.tenco.group3.model.Subject;
+
+public interface SubjectRepository {
+	// 강의 시간표 조회
+	List<Subject> getSubjectAll();
+	
+	List<Subject> getSubjectByType(String type);
+	
+	List<Subject> getSubjectByDeptId(int deptId);
+	
+	List<Subject> getSubjectByName(String name);
+	
+	List<Subject> getSubjectByDeptIDAndType(int deptId, String type);
+	
+	// 수강 신청
+	Subject subjectEnrolment(int studentId, Subject subject);
+}
