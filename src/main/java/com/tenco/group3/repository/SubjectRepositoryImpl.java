7e12b7b8fee9c548e081fa93013df806ac947f0e
junsoo <167273988+junsoo186@users.noreply.github.com>
2024-07-18 17:52:58 +0900

    SubjectRepositoryImpl 미완성 추가

--- src/main/java/com/tenco/group3/repository/SubjectRepositoryImpl.java
+++ src/main/java/com/tenco/group3/repository/SubjectRepositoryImpl.java
@@ -11,13 +11,33 @@ import com.tenco.group3.repository.interfaces.SubjectRepository;
 import com.tenco.group3.util.DBUtil;
 
 public class SubjectRepositoryImpl implements SubjectRepository {
-
-	private final String GET_SUBJECT_BY_SEMESTER = "SELECT * FROM subject WHERE and PROFESSOR_ID= ?  semester = ?";
+	private final String SELECT_ALL_SUBJECT = "select * from subject_tb";
+	private final String GET_SUBJECT_BY_SEMESTER = "SELECT * FROM subject_tb WHERE  PROFESSOR_ID= ? AND AND sub_year = ? AND  semester = ?";
 
 	@Override
 	public List<Subject> getSubjectAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Subject> subjectList = new ArrayList();
+		try (Connection conn = DBUtil.getConnetion();
+				PreparedStatement pstmt = conn.prepareStatement(SELECT_ALL_SUBJECT)){
+				ResultSet rs = pstmt.executeQuery();
+				while (rs.next()) {
+					subjectList.add(Subject.builder()
+							.id(rs.getInt("id"))
+							.name(rs.getString("name"))
+							.professorId(rs.getInt("professorId"))
+							.roomId(rs.getString("roomId"))
+							.deptId(rs.getInt("deptId"))
+							.type(rs.getString("type"))
+							.subYear(rs.getInt("subYear"))
+							.startTime(rs.getInt("startTime"))
+							.endTime(rs.getInt("endTime"))
+							.build()
+							);
+				}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return subjectList;
 	}
 
 	@Override
@@ -25,41 +45,37 @@ public class SubjectRepositoryImpl implements SubjectRepository {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	
+
 	@Override
-	public List<Subject> getSubjectBySemester(int professorId, int semester) {
+	public List<Subject> getSubjectBySemester(int professorId, int subYear, int semester) {
 		List<Subject> subjectList = new ArrayList();
 		try (Connection conn = DBUtil.getConnetion();
-				PreparedStatement pstmt = conn.prepareStatement(GET_SUBJECT_BY_SEMESTER)){
+				PreparedStatement pstmt = conn.prepareStatement(GET_SUBJECT_BY_SEMESTER)) {
 			pstmt.setInt(1, professorId);
-			pstmt.setInt(2, semester);
+			pstmt.setInt(2, subYear);
+			pstmt.setInt(3, semester);
 			ResultSet rs = pstmt.executeQuery();
-			while(rs.next()) {
-				subjectList.add(Subject.builder()
+			while (rs.next()) {
+				subjectList.add(
+						Subject.builder()
 						.id(rs.getInt("id"))
 						.deptId(rs.getInt("deptId"))
 						.name(rs.getString("name"))
 						.startTime(rs.getInt("startTime"))
 						.endTime(rs.getInt("endTime"))
-						.build()
-						);
+						.build());
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return subjectList;
-		
-	} 
-	
 
+	}
 
 	@Override
 	public List<Subject> getStudentBySubject(int id, String type) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-	
 
 }
--- src/main/java/com/tenco/group3/repository/interfaces/SubjectRepository.java
+++ src/main/java/com/tenco/group3/repository/interfaces/SubjectRepository.java
@@ -7,11 +7,11 @@ import com.tenco.group3.model.Subject;
 public interface SubjectRepository {
 	// 강의 시간표 조회
 	List<Subject> getSubjectAll();
-	
+
 	List<Subject> getSubjectByType(String type);
-	
-	List<Subject> getSubjectBySemester(int professorId, int semester);
-	
+
+	List<Subject> getSubjectBySemester(int professorId, int subYear, int semester);
+
 	List<Subject> getStudentBySubject(int id, String type);
-	
+
 }
