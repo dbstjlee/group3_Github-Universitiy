e995f3da752e5bc4bc8c66a3b9fd15e04f51a1b8
ryeonng <zrkfudz29@naver.com>
2024-07-19 17:56:22 +0900

    직원 단과대학 기능2

---
+++ src/main/java/com/tenco/group3/controller/AdminController.java
@@ -0,0 +1,29 @@
+package com.tenco.group3.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/admin/*")
+public class AdminController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	
+    public AdminController() {
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
