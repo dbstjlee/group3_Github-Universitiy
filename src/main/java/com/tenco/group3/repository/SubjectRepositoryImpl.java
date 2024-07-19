6b99bb5898ac43ec648d756844fde0e26653a35e
junsoo <167273988+junsoo186@users.noreply.github.com>
2024-07-19 11:36:05 +0900

    ProfessorRepository

--- src/main/java/com/tenco/group3/repository/SubjectRepositoryImpl.java
+++ src/main/java/com/tenco/group3/repository/SubjectRepositoryImpl.java
@@ -13,27 +13,27 @@ import com.tenco.group3.util.DBUtil;
 public class SubjectRepositoryImpl implements SubjectRepository {
 	private final String SELECT_ALL_SUBJECT = "select * from subject_tb";
 	private final String GET_SUBJECT_BY_SEMESTER = "SELECT * FROM subject_tb WHERE  PROFESSOR_ID= ? AND AND sub_year = ? AND  semester = ?";
-
+	private final String SELECT_SUBJECT_BY_ID = "SELECT * FROM subject_tb WHERE id =AND AND sub_year = ? AND  semester = ?";
+	private final String SELECT_ID_BY_LESS_NUM_STUDENT="SELECT id FROM subject_tb WHERE capacity >= num_of_student ";
 	@Override
 	public List<Subject> getSubjectAll() {
 		List<Subject> subjectList = new ArrayList();
 		try (Connection conn = DBUtil.getConnection();
-				PreparedStatement pstmt = conn.prepareStatement(SELECT_ALL_SUBJECT)){
-				ResultSet rs = pstmt.executeQuery();
-				while (rs.next()) {
-					subjectList.add(Subject.builder()
-							.id(rs.getInt("id"))
-							.name(rs.getString("name"))
-							.professorId(rs.getInt("professorId"))
-							.roomId(rs.getString("roomId"))
-							.deptId(rs.getInt("deptId"))
-							.type(rs.getString("type"))
-							.subYear(rs.getInt("subYear"))
-							.startTime(rs.getInt("startTime"))
-							.endTime(rs.getInt("endTime"))
-							.build()
-							);
-				}
+				PreparedStatement pstmt = conn.prepareStatement(SELECT_ALL_SUBJECT)) {
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				subjectList.add(Subject.builder()
+						.id(rs.getInt("id"))
+						.name(rs.getString("name"))
+						.professorId(rs.getInt("professorId"))
+						.roomId(rs.getString("roomId"))
+						.deptId(rs.getInt("deptId"))
+						.type(rs.getString("type"))
+						.subYear(rs.getInt("subYear"))
+						.startTime(rs.getInt("startTime"))
+						.endTime(rs.getInt("endTime"))
+						.build());
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -74,6 +74,40 @@ public class SubjectRepositoryImpl implements SubjectRepository {
 
 	@Override
 	public List<Subject> getStudentBySubject(int id, String type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Subject> getSubjectById(int id, int year, int semester) {
+		List<Subject> subjectList = new ArrayList();
+		try (Connection conn = DBUtil.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(SELECT_SUBJECT_BY_ID)) {
+			pstmt.setInt(1, id);
+			pstmt.setInt(2, year);
+			pstmt.setInt(3, semester);
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				subjectList.add(Subject.builder()
+						.id(rs.getInt("id"))
+						.name(rs.getString("name"))
+						.professorId(rs.getInt("professorId"))
+						.roomId(rs.getString("roomId"))
+						.deptId(rs.getInt("deptId"))
+						.type(rs.getString("type"))
+						.subYear(rs.getInt("subYear"))
+						.startTime(rs.getInt("startTime"))
+						.endTime(rs.getInt("endTime"))
+						.build());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return subjectList;
+	}
+
+	@Override
+	public List<Subject> selectIdByLessNumOfStudent() {
 		// TODO Auto-generated method stub
 		return null;
 	}
--- src/main/java/com/tenco/group3/repository/interfaces/ProfessorRepository.java
+++ src/main/java/com/tenco/group3/repository/interfaces/ProfessorRepository.java
@@ -1,10 +1,15 @@
 package com.tenco.group3.repository.interfaces;
 
+import java.util.List;
+
 import com.tenco.group3.model.Professor;
 
 public interface ProfessorRepository {
 
-	Professor getProfessorById(int id);
 	int updateProfessorPassword(Professor professor,String id);
-	
+	int updateProfessor(Professor professor,String id);
+	List<Professor> getProfessorList ();
+	Professor getProfessorById(int id);
+	Professor getProfessorByDepartmentId (int deptId);
+	Professor getProfessorAmount();
 }
